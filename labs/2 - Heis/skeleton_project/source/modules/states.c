6b3d3eb62c94bca824e4e5799213a130e33bd1ec
erikbohne <eriknbohne@gmail.com>
2025-02-21 10:20:50 +0100

    floors

--- labs/2 - Heis/skeleton_project/source/modules/states.c
+++ labs/2 - Heis/skeleton_project/source/modules/states.c
@@ -5,20 +5,22 @@
 
 void standing_still(state_data *data, int floor) {
     int i;
-    // Check if any up hall call is pressed on floors above the current floor
+    // Check if any up hall call or cab call is pressed on floors above the current floor
     for (i = floor + 1; i <= 3; i++) {
-        if (data->btnStates[i][1] == 1) {
-            data->btnStates[i][1] = 0;
+        if (data->btnStates[i][BUTTON_HALL_UP] == 1 || data->btnStates[i][BUTTON_CAB] == 1) {
+            data->btnStates[i][BUTTON_HALL_UP] = 0;
+            data->btnStates[i][BUTTON_CAB] = 0;
             printf("Going up from floor %d to %d\n", floor, i);
             elevio_motorDirection(DIRN_UP);
             data->state = DRIVING_UP;
             return;
         }
     }
-    // Check if any down hall call is pressed on floors below the current floor
+    // Check if any down hall call or cab call is pressed on floors below the current floor
     for (i = floor - 1; i >= 0; i--) {
-        if (data->btnStates[i][0] == 1) {
-            data->btnStates[i][0] = 0;
+        if (data->btnStates[i][BUTTON_HALL_DOWN] == 1 || data->btnStates[i][BUTTON_CAB] == 1) {
+            data->btnStates[i][BUTTON_HALL_DOWN] = 0;
+            data->btnStates[i][BUTTON_CAB] = 0;
             printf("Going down from floor %d to %d\n", floor, i);
             elevio_motorDirection(DIRN_DOWN);
             data->state = DRIVING_DOWN;
@@ -38,10 +40,13 @@ void open_door(state_data *data){
 void driving_up(state_data *data, int floor){
 
     if (floor >= 0){
-        if(data->btnStates[floor][1] == 1){
-            data->btnStates[floor][1] = 0;
+        if(data->btnStates[floor][BUTTON_HALL_UP] == 1){
+            data->btnStates[floor][BUTTON_HALL_UP] = 0;
             printf("Stopped at floor %d\n", floor);
             elevio_motorDirection(DIRN_STOP);
+            elevio_buttonLamp(floor, BUTTON_HALL_UP, 0);
+            elevio_buttonLamp(floor, BUTTON_HALL_DOWN, 0);
+            elevio_buttonLamp(floor, BUTTON_CAB, 0);
             open_door(data);
             data->state = STANDING_STILL;
         }
@@ -56,10 +61,13 @@ void driving_up(state_data *data, int floor){
 
 void driving_down(state_data *data, int floor){
     if(floor >= 0){
-        if(data->btnStates[floor][0] == 1){
-            data->btnStates[floor][0] = 0;
+        if(data->btnStates[floor][BUTTON_HALL_DOWN] == 1){
+            data->btnStates[floor][BUTTON_HALL_DOWN] = 0;
             printf("Stopped at floor %d\n", floor);
             elevio_motorDirection(DIRN_STOP);
+            elevio_buttonLamp(floor, BUTTON_HALL_UP, 0);
+            elevio_buttonLamp(floor, BUTTON_HALL_DOWN, 0);
+            elevio_buttonLamp(floor, BUTTON_CAB, 0);
             open_door(data);
             data->state = STANDING_STILL;
             return;

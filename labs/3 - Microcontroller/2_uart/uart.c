ef85cb551cfac4bbec7228422eaa259e0d60e7e4
mariusrena <mariusrena@outlook.com>
2025-03-28 11:44:19 +0100

    Generated code, not working

--- labs/3 - Microcontroller/2_uart/uart.c
+++ labs/3 - Microcontroller/2_uart/uart.c
@@ -4,37 +4,58 @@
 #define UART ((NRF_UART_REG*)0x40002000)
 
 typedef struct {
-    volatile uint32_t RESERVED0[3];
-    volatile uint32_t TASKS_STARTRX;
-    volatile uint32_t TASKS_STOPRX;
-    volatile uint32_t TASKS_STARTTX;
-    volatile uint32_t TASKS_STOPTX;
-    volatile uint32_t RESERVED1[56];
-    volatile uint32_t EVENTS_CTS;
-    volatile uint32_t EVENTS_NCTS;
-    volatile uint32_t EVENTS_RXDRDY;
-    volatile uint32_t RESERVED2[4];
-    volatile uint32_t EVENTS_TXDRDY;
-    volatile uint32_t RESERVED3[7];
-    volatile uint32_t EVENTS_ERROR;
-    volatile uint32_t RESERVED4[46];
-    volatile uint32_t SHORTS;
-    volatile uint32_t RESERVED5[64];
-    volatile uint32_t INTENSET;
-    volatile uint32_t INTENCLR;
-    volatile uint32_t RESERVED6[93];
-    volatile uint32_t ERRORSRC;
-    volatile uint32_t RESERVED7[31];
-    volatile uint32_t ENABLE;
-    volatile uint32_t RESERVED8[1];
-    volatile uint32_t PSELRTS;
-    volatile uint32_t PSELTXD;
-    volatile uint32_t PSELCTS;
-    volatile uint32_t PSELRXD;
-    volatile uint32_t RXD;
-    volatile uint32_t TXD;
-    volatile uint32_t RESERVED9[17];
-    volatile uint32_t BAUDRATE;
+    volatile uint32_t TASKS_STARTRX;    // 0x000 Start UART receiver
+    volatile uint32_t TASKS_STOPRX;     // 0x004 Stop UART receiver
+    volatile uint32_t TASKS_STARTTX;    // 0x008 Start UART transmitter
+    volatile uint32_t TASKS_STOPTX;     // 0x00C Stop UART transmitter
+    volatile uint32_t RESERVED0[3];     // Padding for 0x010 - 0x018
+    volatile uint32_t TASKS_SUSPEND;    // 0x01C Suspend UART
+    
+    volatile uint32_t RESERVED1[7];     // Padding for 0x020 - 0x0FC
+    
+    volatile uint32_t EVENTS_CTS;       // 0x100 CTS is activated (set low). Clear To Send.
+    volatile uint32_t EVENTS_NCTS;      // 0x104 CTS is deactivated (set high). Not Clear To Send.
+    volatile uint32_t EVENTS_RXDRDY;    // 0x108 Data received in RXD
+    volatile uint32_t RESERVED2[1];     // Padding for 0x10C - 0x118
+    volatile uint32_t EVENTS_TXDRDY;    // 0x11C Data sent from TXD
+    volatile uint32_t EVENTS_ERROR;     // 0x124 Error detected
+    volatile uint32_t RESERVED3[3];     // Padding for 0x128 - 0x140
+    volatile uint32_t EVENTS_RXTO;      // 0x144 Receiver timeout
+    
+    volatile uint32_t RESERVED4[3];     // Padding for 0x148 - 0x1FC
+    
+    volatile uint32_t SHORTS;           // 0x200 Shortcut register
+    
+    volatile uint32_t RESERVED5[12];    // Padding for 0x204 - 0x2FC
+    
+    volatile uint32_t INTENSET;         // 0x304 Enable interrupt
+    volatile uint32_t INTENCLR;         // 0x308 Disable interrupt
+    
+    volatile uint32_t RESERVED6[28];    // Padding for 0x30C - 0x47C
+    
+    volatile uint32_t ERRORSRC;         // 0x480 Error source
+    
+    volatile uint32_t RESERVED7[7];     // Padding for 0x484 - 0x4FC
+    
+    volatile uint32_t ENABLE;           // 0x500 Enable UART
+    
+    volatile uint32_t RESERVED8[3];     // Padding for 0x504 - 0x508
+    
+    volatile uint32_t PSELRTS;          // 0x508 Pin select for RTS
+    volatile uint32_t PSELTXD;          // 0x50C Pin select for TXD
+    volatile uint32_t PSELCTS;          // 0x510 Pin select for CTS
+    volatile uint32_t PSELRXD;          // 0x514 Pin select for RXD
+    
+    volatile uint32_t RESERVED9[2];     // Padding for 0x518 - 0x520
+    
+    volatile uint32_t RXD;              // 0x518 RXD register
+    volatile uint32_t TXD;              // 0x51C TXD register
+    
+    volatile uint32_t BAUDRATE;         // 0x524 Baud rate
+    
+    volatile uint32_t RESERVED10[10];   // Padding for 0x528 - 0x56C
+    
+    volatile uint32_t CONFIG;  
 } NRF_UART_REG;
 
 void uart_init() {
